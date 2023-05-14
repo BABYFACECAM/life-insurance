1b904c67cedf819b9f34ecae506b75bc6cc30ab2
BABYFACECAM <babyfacecam@icloud.com>
2023-05-14 11:06:34 -0500

    completed testimonials component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,7 +28,7 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   return (
     <>
       <Head>
-        <title>Mantine next example</title>
+        <title>Barnett Legacy Life Insurance</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
